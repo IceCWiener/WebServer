400a5883c52c7425ecf51b73db0f29e098c98dde
Konstantin Regenhardt <konstantinlucius.regenhardt@cgi.com>
2022-06-07 12:58:49 +0200

    added gson functionality

---
+++ server/Person.java
@@ -0,0 +1,12 @@
+package server;
+
+public class Person {
+
+  Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  String name;
+  int age;
+}
--- server/WebServer.java
+++ server/WebServer.java
@@ -1,18 +1,20 @@
 package server;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
-import com.google.gson.Gson;
 
 /**
  * @author Gerasimos Strecker & Konstantin Regenhardt
  */
 
 public class WebServer {
+  private Person person = new Person("Claudia", 44);
 
   public static void main(String args[]) throws IOException {
     try (ServerSocket server = new ServerSocket(8080)) {
@@ -38,5 +40,12 @@ public class WebServer {
         }
       }
     }
+  }
+
+  public String constructJson() {
+    Gson gson = new Gson();
+
+    String json = gson.toJson(person);
+    return json;
   }
 }
