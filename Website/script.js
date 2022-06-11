433efe62344ece7b03bb39d339cdfbd34d3dab13
TreeSparrow69 <treesparrow69@protonmail.com>
2022-06-11 15:18:05 +0200

    request to json

--- Website/script.js
+++ Website/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   console.log("stop inspecting element");
   var receivedHTML = "Platzhalter";
   $("#button").on("click", function () {
-    
+    document.getElementById("containerHTML").focus();
     $("#containerHTML").text(createRequest());
     $("#containerHTML").removeClass("disabled");
   });
@@ -20,13 +20,20 @@ $(document).ready(function () {
     }
   }
   function createRequest() {
-    var methods = document.getElementById("Methods").value;
+    var method = document.getElementById("Methods").value;
     var path = document.getElementById("pathInput").value;
     var urlInput = document.getElementById("host").value;
     var protocol = document.getElementById("protocolInput").value;
     var accept = document.getElementById("acceptInput").value;
     var format = document.getElementById("jsonXML").value;
-    console.log(methods, path, urlInput, protocol, accept, format);
-    return methods + path + urlInput + protocol + accept + format;
+    var combined = {
+      method: method,
+      path: path,
+      urlInput: urlInput,
+      protocol: protocol,
+      accept: accept,
+      format: format,
+    };
+    return JSON.stringify(combined);
   }
 });
