2b93206f309568d3107e8d0a95c41ccd90b4a4dd
Konstantin Regenhardt <konstantinlucius.regenhardt@cgi.com>
2022-06-12 15:46:15 +0200

    changed getIp, added requestmethods

--- server/WebServer.java
+++ server/WebServer.java
@@ -1,9 +1,12 @@
 package server;
 
 import com.google.gson.Gson;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
@@ -11,15 +14,20 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.Date;
 import java.util.Enumeration;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * @author Gerasimos Strecker & Konstantin Regenhardt
  */
 
 public class WebServer {
-  private static Person person = new Person("Claudia", 44);
+  private static Person person1 = new Person("Claudia", 44);
   private static Person person2 = new Person("Clarence", 63);
-  private static Person[] people = { person, person2 };
+  private static Person[] people = { person1, person2 };
 
   public static void main(String args[]) throws IOException {
     try (ServerSocket server = new ServerSocket(8080)) {
@@ -36,20 +44,23 @@ public class WebServer {
             "\nVerbundener Client: " + client.getInetAddress().getHostAddress()
           );
 
-          DataInputStream input = new DataInputStream(client.getInputStream());
-          System.out.println("Request des Clients" + input.readUTF());
+          handleClient(client);
+          System.out.println("Send Output:");
+          sendOutput(client);
+          // DataInputStream input = new DataInputStream(client.getInputStream());
+          // System.out.println("Request des Clients" + input.readUTF());
 
-          DataOutputStream output = new DataOutputStream(
-            client.getOutputStream()
-          );
-          String httpResponse =
-            // "Sie haben sich mit dem Server um " +
-            // today +
-            // " Verbunden\n Antwort auf Ihren request:\n" +
-            constructJson(people);
-          output.writeUTF(httpResponse);
+          // DataOutputStream output = new DataOutputStream(
+          //   client.getOutputStream()
+          // );
+          // String httpResponse =
+          //   // "Sie haben sich mit dem Server um " +
+          //   // today +
+          //   // " Verbunden\n Antwort auf Ihren request:\n" +
+          //   constructJson(people);
+          // output.writeUTF(httpResponse);
 
-          client.close();
+          // client.close();
         }
       }
     }
@@ -68,7 +79,7 @@ public class WebServer {
         InetAddress i = inetAddresses.nextElement();
         address = i.getHostAddress();
         split = address.split("\\.");
-        if (split[0].equals("192")) {
+        if (split[0].equals("192") || split[0].equals("10")) {
           localIp = address;
           break;
         }
@@ -91,5 +102,45 @@ public class WebServer {
 
   public String receiveRequest(String json) {
     return "";
+  }
+
+  public Response createHttpResponse() throws IOException {
+    OkHttpClient client = new OkHttpClient().newBuilder().build();
+    MediaType mediaType = MediaType.parse("text/plain");
+    RequestBody body = RequestBody.create("message", mediaType);
+    Request request = new Request.Builder()
+      .url("192.168.0.24:8080")
+      .method("POST", body)
+      .build();
+    Response response = client.newCall(request).execute();
+
+    return response;
+  }
+
+  private static void handleClient(Socket client) throws IOException {
+    System.out.println("Debug: got new client " + client.toString());
+    BufferedReader br = new BufferedReader(
+      new InputStreamReader(client.getInputStream())
+    );
+
+    StringBuilder requestBuilder = new StringBuilder();
+    String line;
+    while (!(line = br.readLine()).isBlank()) {
+      requestBuilder.append(line + "\r\n");
+    }
+
+    String request = requestBuilder.toString();
+    System.out.println(request);
+  }
+
+  public static void sendOutput(Socket client) throws IOException {
+    OutputStream clientOutput = client.getOutputStream();
+    clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
+    clientOutput.write(("ContentType: text/html\r\n").getBytes());
+    clientOutput.write("\r\n".getBytes());
+    clientOutput.write("<b>It works!</b>".getBytes());
+    clientOutput.write("\r\n\r\n".getBytes());
+    clientOutput.flush();
+    client.close();
   }
 }
