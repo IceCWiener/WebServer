e50691d0027fc297ff9e60b5916b39aa9e98082b
Konstantin Regenhardt <konstantin.regenhardt@gmx.net>
2022-07-10 13:54:09 +0200

    cleaned code

--- server/WebServer.java
+++ server/WebServer.java
@@ -13,15 +13,8 @@ import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
-import java.time.format.DateTimeFormatter;
 import java.util.Enumeration;
 import java.util.Scanner;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import okhttp3.MediaType;
@@ -29,19 +22,15 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.internal.http2.Http2Stream.StreamTimeout;
 
 /**
  * @author Gerasimos Strecker & Konstantin Regenhardt
  */
 
 public class WebServer {
-  private static String html = "<!DOCTYPE html> <html> <head> <title>Verteilte Systeme</title> <meta name='keywords' content='Vortrag' /> <link rel='stylesheet' href='style.css' /> <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script> <style> input { padding: 15px; height: 1em; min-width: 20px; } #outterWrapper { display: flex; flex-direction: column; height: 100%; width: 100%; justify-content: space-evenly; align-items: center; position: absolute; }#wrapper { /* top: 50%; left: 50%; transform: translate(-50%, -50%); */ display: flex; flex-direction: column; gap: 20px; transition: all 0.5s ease-in-out; -webkit-transition: all 0.5s ease-in-out; background-color: lightgoldenrodyellow; padding: 15px; border-radius: 15px; } #box { margin: 0 50px; display: flex; flex-direction: column; width: fit-content; } .rows { display: flex; gap: 15px; } .rowElement { display: flex; flex-direction: column; } input, select, #button, #containerHTML { box-shadow: 5px 5px 7px rgb(171, 171, 171); border-radius: 15px; font-size: 2em; } ::placeholder { color: lightgray; }#Methods, #jsonXML, #button { height: 2em; } #button { width: 150px; background-color: #e248c0; align-self: baseline; } #containerHTML { background-color: lightgoldenrodyellow; width: calc(80%); height: fit-content; opacity: 1; padding: 15px; overflow-wrap: break-word; } #protocolInput, #acceptInput { background-color: white; }body { background-color: antiquewhite; /*background: url(https://cdn.dribbble.com/users/652916/screenshots/1925630/explosion_01_dribbble.gif); */ } .disabled { display: none; transition: opacity 0.5s ease-in-out; opacity: 0; } * { font-family: monospace, monospace; } .empty { flex-grow: 1; } </style> </head> <body> <div id='outterWrapper'> <div id='wrapper'> <h1 id='header'>Header:</h1> <div id='box'> <div class='rows'> <div class='rowElement'> <h1>Crud:</h1> <select name='Methods' id='Methods'> <!--<option value='' disabled selected>GET</option>--> <option value='GET'>GET</option> <option value='POST'>POST</option> <option value='POST'>PUT</option> <option value='POST'>DELETE</option> </select> </div> <div class='rowElement'> <h1>Path:</h1> <input type='text' placeholder='/<your Path>' id='pathInput' /> </div> </div> <div class='rows'> <div class='rowElement'> <h1>Host:</h1> <input type='text' id='host' placeholder='enter URL' /> </div> <div class='rowElement'> <h1>Protocol:</h1> <input type='text' value='HTTP/1.1' id='protocolInput' disabled /> </div> </div> <div class='rows'> <div class='rowElement'> <h1>Accept:</h1> <input type='text' value='application/' id='acceptInput' disabled /> </div> <div class='rowElement'> <h1>Format:</h1> <select id='jsonXML'> <option value='json'>JSON</option> <option value='xml'>XML</option> </select> </div> <div class='rowElement'> <h1 class='empty'></h1> <button id='button'>GO</button> </div> </div> </div> </div> <div id='containerHTML' class='disabled'></div> </div> </body> <script type='text/javascript'> $(document).ready(function () { console.log('stop inspecting element'); $('#button').on('click', function () { var method = document.getElementById('Methods').value; var path = document.getElementById('pathInput').value; var protocol = document.getElementById('protocolInput').value; var accept = document.getElementById('acceptInput').value; var format = document.getElementById('jsonXML').value; var urlInput = document.getElementById('host').value; document.getElementById('containerHTML').focus(); $('#containerHTML').text( createRequest(method, path, urlInput, protocol, accept, format) ); $('#containerHTML').removeClass('disabled');var url = new URL('ws://' + urlInput + ':8080');console.log(url); var socket = new WebSocket(url); console.log('socket:' + socket);console.log(socket.data);socket.onopen = function (e) { open = true; console.log('test'); socket.send( createRequest(method, path, urlInput, protocol, accept, format) ); };socket.onmessage = function (ev) { alert(ev.data); }; }); $('input').each(function () { var input = this; /* This is the jquery object of the input, do what you will*/ input.addEventListener('input', resizeInput); /* bind the 'resizeInput' callback on 'input' event*/ resizeInput.call(input); /* immediately call the function*/ });function resizeInput() { if (this.value.length == 0) { this.style.width = this.placeholder.length + 'ch'; } else { this.style.width = this.value.length + 'ch'; } /*console.log($('#containerHTML')[0], $('#outterWrapper')[0].clientWidth);*/ $('#containerHTML')[0].clientWidth = $('#outterWrapper')[0].clientWidth; /*console.log($('#containerHTML')[0].style.width, $('#outterWrapper'));*/ } function createRequest(method, path, urlInput, protocol, accept, format) { var combined = { method: method, path: path, urlInput: urlInput, protocol: protocol, accept: accept, format: format, }; return JSON.stringify(combined); } }); </script> </html> ";
-  private static String testJson = "{\"name\":\"kaewin\"}";
   private static Gson gson = new Gson();
 
   public static void main(String args[]) throws IOException {
-    // readFile(".\\files\\person1.txt");
     try (ServerSocket server = new ServerSocket(8080)) {
       System.out.println(
           "Listening for connection on:\n " +
@@ -61,10 +50,9 @@ public class WebServer {
   }
 
   private static void receiveRequest(Socket client) throws IOException {
-    // System.out.println("Debug: got new client " + client.toString());
     BufferedReader br = new BufferedReader(
         new InputStreamReader(client.getInputStream()));
-    String request = br.readLine(); // Now you get GET index.html HTTP/1.1
+    String request = br.readLine();
     String[] requestParam = request.split(" ");
     String method = requestParam[0];
     String path = "." + requestParam[1];
@@ -73,54 +61,28 @@ public class WebServer {
       String line = br.readLine();
       String message = "";
 
-      // Scannertest
-      // Scanner sc = new Scanner(client.getInputStream());
-      // while(sc.hasNext()){
-      // content.append(sc.next());
-      // content.append(System.lineSeparator());
-      // message = content.toString();
-      // }
-      // sc.close();
-
-      // BufferedReader // only works with an empty line at the end of msg
-      // for (int i = 0; i < 8; i++) {
-      //   br.readLine();
-      // }
-      // while ((!(line = br.readLine()).isEmpty())) {
-      //   content.append(line);
-      //   content.append(System.lineSeparator());
-      //   message = content.toString();
-      // }
-
-      // Br with curly counter
+      // Br with curly counter //Body needs empty line at he end
       for (int i = 0; i < 8; i++) {
         br.readLine();
       }
-      while ((!(line = br.readLine()).isEmpty())) {
+      int count1 = 0;
+      int count2 = 0;
+
+      while (((line = br.readLine()) != null)) {
+        String str = line.toString();
+        if (str.contains("{")) {
+          count1++;
+        }
+        if (str.contains("}")) {
+          count2++;
+        }
+        if (count1 == count2) {
+          break;
+        }
         content.append(line);
         content.append(System.lineSeparator());
         message = content.toString();
       }
-
-      // Collection reader
-      // message = readAllLinesWithStream(br);
-
-      // Timout test
-      // Future<String> future = CompletableFuture.supplyAsync(() ->
-      // readAllLinesWithStream(br));
-      // String msg = "";
-      // try{
-      // msg = future.get(1, TimeUnit.SECONDS);
-      // createFile(msg);
-      // } catch(TimeoutException e) {
-      // System.out.println("Timeout of method");
-      // future.cancel(true);
-      // } catch(InterruptedException | ExecutionException e) {
-      // System.out.println(e);
-      // }
-
-      // System.out.println(message);
-      // String[] pruned = message.split("\n");
 
       createFile(message);
       sendResponse(client, "Inhalte wurden gespeichert.");
@@ -130,8 +92,23 @@ public class WebServer {
     }
   }
 
-  private static String readAllLinesWithStream(BufferedReader br) {
-    return br.lines().collect(Collectors.joining(System.lineSeparator()));
+  private static void createFile(String text) {
+    String name = "file" + (new File(".\\files").list().length + 1);
+
+    try {
+      File file = new File(".//files//" + name + ".txt");
+      if (file.createNewFile()) {
+        System.out.println("Neuen Eintrag erstellt: " + file.getName());
+      } else {
+        System.out.println("Dateiname schon vergeben.");
+      }
+
+      FileWriter writer = new FileWriter(file);
+      writer.write(text);
+      writer.close();
+    } catch (IOException error) {
+      System.out.println("IOError:\n" + error);
+    }
   }
 
   public static void sendResponse(Socket client, String body)
@@ -139,22 +116,29 @@ public class WebServer {
     OutputStream clientOutput = client.getOutputStream();
 
     clientOutput.write("HTTP/1.1 200 OK".getBytes());
-    // clientOutput.write("Date: Mon, 27 Jul 2009 12:28:53 GMT".getBytes());
-    // clientOutput.write("Server: Apache/2.2.14 (Win32)".getBytes());
-    // clientOutput.write("Last-Modified: Wed, 22 Jul 2009 19:15:56
-    // GMT".getBytes());
-    // clientOutput.write("Content-Length: 88".getBytes());
     clientOutput.write("Content-Type: text/plain\r\n".getBytes());
-    // clientOutput.write("\n".getBytes());
-    // clientOutput.write("test".getBytes());
-
-    // clientOutput.write("HTTP/1.1 200 OK\r\n".getBytes());
-    // clientOutput.write(("ContentType: text/html\r\n").getBytes());
     clientOutput.write("\r\n".getBytes());
     clientOutput.write(body.getBytes());
-    // clientOutput.write(html.getBytes());
     clientOutput.write("\r\n\r\n".getBytes());
     clientOutput.write("Connection: Closed\r\n".getBytes());
+  }
+
+  private static String readFile(String path) {
+    String content = "";
+    try {
+      File file = new File(path);
+      Scanner sc = new Scanner(file);
+      while (sc.hasNextLine()) {
+        content = content + sc.nextLine();
+        // System.out.println(content);
+      }
+      sc.close();
+    } catch (FileNotFoundException error) {
+      System.out.println("Pfad wurde nicht gefunden.");
+      // error.printStackTrace();
+    }
+
+    return content;
   }
 
   private static String getLocalIp() throws SocketException {
@@ -206,40 +190,7 @@ public class WebServer {
     return response;
   }
 
-  private static void createFile(String text) {
-    String name = "file" + (new File(".\\files").list().length + 1);
-
-    try {
-      File file = new File(".//files//" + name + ".txt");
-      if (file.createNewFile()) {
-        System.out.println("Neuen Eintrag erstellt: " + file.getName());
-      } else {
-        System.out.println("Dateiname schon vergeben.");
-      }
-
-      FileWriter writer = new FileWriter(file);
-      writer.write(text);
-      writer.close();
-    } catch (IOException error) {
-      System.out.println("IOError:\n" + error);
-    }
-  }
-
-  private static String readFile(String path) {
-    String content = "";
-    try {
-      File file = new File(path);
-      Scanner sc = new Scanner(file);
-      while (sc.hasNextLine()) {
-        content = content + sc.nextLine();
-        // System.out.println(content);
-      }
-      sc.close();
-    } catch (FileNotFoundException error) {
-      System.out.println("Pfad wurde nicht gefunden.");
-      // error.printStackTrace();
-    }
-
-    return content;
+  private static String readAllLinesWithStream(BufferedReader br) {
+    return br.lines().collect(Collectors.joining(System.lineSeparator()));
   }
 }
